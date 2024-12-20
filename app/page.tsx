f26f4c0c23746c7a0eb19a22234eb8c3b704ca9c
Alex Aylward <alexaylward@outlook.com>
2024-12-20 17:41:11 -0500

    all images render NOW

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/personal-site/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/personal-site/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -88,7 +88,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/personal-site/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
